1d73202beff2baa53755e318749fd52145b07b02
“Marco <marco.garciar08@gmail.com>
2021-05-01 10:03:10 -0500

    Comentarios, sentencias, variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,73 @@
+
+//Hola mundo
+alert('Soy JavaScript!');
+
+//Comentarios
+
+//comentarios en una linea
+/*Comentatios
+en mas de una
+linea*/
+
+
+
+//Sentencias
+
+//alert('Hola'); alert('Mundo');
+
+//alert('Hola');
+//alert('Mundo');
+
+//INSERCIÓN AUTOÁTICA DE PUNTO Y COMA
+
+//alert('Hola')
+//alert('Mundo')
+
+//Variables
+
+let mensaje;
+mensaje="Hola mundo"
+alert(mensaje);
+
+let a=5;
+let b=8;
+let suma = a +b;
+console.log("El resultado es: ",suma);
+
+let nombre ="Marco";
+let nombre2=nombre;
+
+nombre2="Antonio";
+
+console.log("El nombre uno es: ",nombre);
+console.log("El nombre dos es: ",nombre2);
+
+// Nombrar variables con camel case
+
+nombreUno ="Marco";
+nombreDos="Antonio";
+
+//Nombrar como underscore
+nombre_uno="Marco";
+nombre_dos="Antonio";
+
+// nombres no comunes
+let $=1;
+let _=2;
+
+//no puede nombrar variables
+
+/*let 1a=2;
+let primer-nombre="Marco";*/
+
+"use strict"
+mensaje="Hola mundo";
+
+//Constantes
+
+const pi =3.141624;
+const COLOR_ROJO = "#FFFFF";
+pi_dos= pi;
+pi_dos=7;
+console.log(pi);
+console.log(pi_dos);
